53292506422371890513c365c69132ec58a27108
StSerana <serana.st19@gmail.com>
2020-12-13 22:53:15 +0500

    Добавьте файлы проекта.

---
+++ Homework7/SLList.h
@@ -0,0 +1,17 @@
+#pragma once
+class SLList
+{
+	private:
+		SLList();
+		SLList(int data);
+	public:
+		SLList* m_nextElem;
+		int m_data = 0;
+		void setData(int data);
+		SLList* next();
+		bool hasNext();
+		static SLList* beginList(int data);
+		SLList* addElement(int data);
+		void removeNext();
+};
+void printSLList(SLList* lis);
