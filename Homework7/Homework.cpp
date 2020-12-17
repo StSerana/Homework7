55df4f54cc546e9739c3f7818980d19490594b5f
StSerana <serana.st19@gmail.com>
2020-12-18 02:21:10 +0500

    add task

--- Homework7/Homework.cpp
+++ Homework7/Homework.cpp
@@ -1,9 +1,27 @@
 ﻿#include <iostream>
 #include <ctime>
+#include "Maze.h"
+#include "MCell.h"
 using namespace std;
+
+Maze* createMaze(Maze* maze); 
 
 int main()
 {
 	srand(time(0));
+	Maze* maze = new Maze(5, 5);
+	maze = createMaze(maze);
 	return 0;
+}
+
+Maze* createMaze(Maze* maze) {
+	int i = 1;
+	int j = 0;
+	while (i * j < 5 * 5) {
+		maze->makeConnection(i, j, i, j + 1);
+		j++;
+		maze->makeConnection(i, j, i + 1, j);
+		i++;
+	}
+	return maze;
 }
